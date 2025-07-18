2ae289a13e357316fbf043443303c77fc5a29f30
AdiI006-yo <aditya.singh3.cs28@iilm.edu>
2025-07-18 14:24:08 +0530

    Update adi.c

--- adi.c
+++ adi.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-int isPrime(int n) {
-    if (n <= 1) return 0;
-    for (int i = 2; i * i <= n; i++)
-        if (n % i == 0) return 0;
-    return 1;
-}
-
 int main() {
-    int n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    printf("%s\n", isPrime(n) ? "Prime" : "Not prime");
+    int n = 2;
+    if (n <= 1) printf("Not prime\n");
+    else if (n == 2) printf("Prime\n");
+    else {
+        for (int i = 2; i < n; i++)
+            if (n % i == 0) { printf("Not prime\n"); return 0; }
+        printf("Prime\n");
+    }
     return 0;
 }
